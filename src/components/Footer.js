a94c9e49fe0963e111ebcb0a84054b2fe6916530
Mark Rattle <mark.rattle@gmail.com>
2023-02-23 17:50:49 -0500

    logo

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+// src/Footer.js
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className='Footer'>
+      <p>Climbunity // Mark Rattle 2023 - for ACS-1320</p>
+    </div>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Header.css';
+import logo from './logo-no-background.png';
+// import { NavLink } from 'react-router-dom';
+
+function Header() {
+  return (
+    <div className='Header'>
+      <header>
+        <img src={logo} alt="Climbunity Logo" />
+        <div className='Header-Subtitle'>
+          Local climbing area information
+        </div>
+        <div className="Header-Buttons">
+          {/* <NavLink 
+            className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
+            to="/"
+            >
+              List
+          </NavLink>
+          <NavLink 
+            className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
+            to="/about"
+            >
+              About
+          </NavLink> */}
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header;
