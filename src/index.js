d012a34f90ea423a8070f20e4a1fdb9797525dd1
Mark Rattle <mark.rattle@gmail.com>
2023-02-23 18:04:55 -0500

    favicon

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <title>Climbunity</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico"/>
     <App />
   </React.StrictMode>
 );
