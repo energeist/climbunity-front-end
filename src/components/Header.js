220716cd3fe846db529a5cf332fd370fc12a8ea1
Mark Rattle <mark.rattle@gmail.com>
2023-03-01 21:23:17 -0500

    back button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import './Header.css';
 import logo from './logo-no-background.png';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate, useLocation } from 'react-router-dom';
 
 function Header() {
+  const handleClick = () => {
+    navigate(-1);
+  };
+  const location = useLocation()
+  const navigate = useNavigate()
+  console.log(location.pathname)
   return (
     <div className='Header'>
       <header>
         <img src={logo} alt="Climbunity Logo" />
         <div className='Header-Subtitle'>
-          Local climbing area information
+          Climbing in the Red River Gorge
         </div>
         <div className="Header-Buttons">
-          <NavLink 
-            className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
-            to="/"
-            >
-              Climbing Areas
-          </NavLink>
-          <NavLink 
-            className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
-            to="/about"
-            >
-              About Climbunity
-          </NavLink>
+          <div>
+            <NavLink 
+              className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
+              to="/"
+              >
+                Climbing Areas
+            </NavLink>
+            <NavLink 
+              className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
+              to="/about"
+              >
+                About Climbunity
+            </NavLink>
+            { location.pathname === '/' ? '' :
+              <NavLink 
+                className="nav-link"
+                onClick={handleClick}
+                >
+                  Go Back
+              </NavLink>
+            }
+          </div>
         </div>
       </header>
     </div>
